16cb921200fc462d3d988b68f6e119738cf301f1
William Gardner <william@madebycounter.com>
2024-06-16 19:45:27 -0700

    About page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,10 @@
 import About from "@/components/pages/About";
 
-import {
-    useAboutPage,
-    useCompanyInfo,
-    usePortfolioItem,
-    useTeamMember,
-} from "@/lib/types";
-
-import Test from "@/app/test";
+import { useAboutPage, useCompanyInfo } from "@/lib/types";
 
 export default async function Page() {
-    const portfolioItem = await usePortfolioItem("margaretfest-2023");
     const companyInfo = await useCompanyInfo();
-    const henry = await useTeamMember("henry-buck");
-    const luke = await useTeamMember("luke-a-makinson");
-    const william = await useTeamMember("william-gardner");
     const aboutPage = await useAboutPage();
 
-    return (
-        // <Test aboutPage={aboutPage} />
-
-        <About
-            portfolioItem={portfolioItem}
-            companyInfo={companyInfo}
-            aboutPage={aboutPage}
-            henry={henry}
-            luke={luke}
-            william={william}
-        />
-    );
+    return <About companyInfo={companyInfo} aboutPage={aboutPage} />;
 }
