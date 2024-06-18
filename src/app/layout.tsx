2e196a7bdfa3623c8f03ddba8e1cdf5a48fcecc9
William Gardner <william@madebycounter.com>
2024-06-17 17:43:02 -0700

    Favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from "next";
-
 import "./globals.css";
-
-export const metadata: Metadata = {
-    title: "Counter | About",
-    description: "Your local media production company.",
-};
 
 export default function RootLayout({
     children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Metadata, ResolvingMetadata } from "next";
+
 import About from "@/components/pages/About";
 
 import { useAboutPage, useCompanyInfo } from "@/lib/types";
 
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "abc",
+        manifest: "/site.webmanifest",
+    };
+}
 export default async function Page() {
     const companyInfo = await useCompanyInfo();
     const aboutPage = await useAboutPage();
