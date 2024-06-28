853e632fcaa148876f9d2a821e505952e0ba7a44
William Gardner <william@madebycounter.com>
2024-06-28 16:12:21 -0700

    Test

--- src/app/api/draft/enable/route.ts
+++ src/app/api/draft/enable/route.ts
@@ -1,12 +1,12 @@
-import { validatePreviewUrl } from "@sanity/preview-url-secret";
+// import { validatePreviewUrl } from "@sanity/preview-url-secret";
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { client } from "@/lib/sanity";
+// import { client } from "@/lib/sanity";
 
-const clientWithToken = client.withConfig({
-    token: process.env.SANITY_READ_TOKEN,
-});
+// const clientWithToken = client.withConfig({
+//     token: process.env.SANITY_READ_TOKEN,
+// });
 
 export async function GET(request: Request) {
     // const { isValid, redirectTo = "/" } = await validatePreviewUrl(
