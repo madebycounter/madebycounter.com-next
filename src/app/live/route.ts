a3aa80daf54781d707d850f9fd2c0cbad76f432e
William Gardner <william@madebycounter.com>
2025-04-07 11:26:15 -0700

    No revalidate cache

--- src/app/live/route.ts
+++ src/app/live/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
 
     var resp = await fetch(
         `${baseUrl}?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`,
-        { next: { revalidate: 30 } },
+        { next: { revalidate: 0 } },
     );
 
     var dat = await resp.json();
