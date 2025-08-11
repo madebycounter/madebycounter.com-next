f3ca344f569ab0b7330eb8c059576f81727dc3aa
William Gardner <william@madebycounter.com>
2025-08-11 11:36:26 -0700

    Added page titles for sign up pages

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from "next";
+
 import { useCompanyInfo, useAboutPage } from "@/lib/query";
 
 import NewsletterPage from "./Newsletter";
+
+export const metadata: Metadata = {
+    title: "Counter | Sign up for our monthly newsletter",
+    description: "The latest /counter updates, direct to your inbox.",
+};
 
 export default async function Page() {
     const companyInfo = await useCompanyInfo();
--- src/app/notify/page.tsx
+++ src/app/notify/page.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from "next";
+
 import { useCompanyInfo, useAboutPage } from "@/lib/query";
 
 import NotifyPage from "./Notify";
+
+export const metadata: Metadata = {
+    title: "Counter | Subscribe to SMS notifications",
+    description: "Get notified when your favorite bands go live!",
+};
 
 export default async function Page() {
     const companyInfo = await useCompanyInfo();
