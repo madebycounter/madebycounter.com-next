eafddfbcafd1351e896bd437691fc90d20f45abc
William Gardner <william@madebycounter.com>
2026-04-16 18:08:13 -0700

    Added archive redirect

---
+++ src/app/archive/route.ts
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+    redirect("https://www.dropbox.com/scl/fo/409jfkdc1e8i6synj19gz/AK3B1wirymSaTU12Wql7MxM?rlkey=2efifo9porwptyqpi9jlf9xaq&st=nfc911v4&dl=0");
+}
